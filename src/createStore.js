2e89ae7e07d768a1aa73ca958ab5ee6063f141f8
ndean87 <ndean87@gmail.com>
2017-11-16 14:05:00 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,29 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action){
+    state= reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  dispatch({type: '@@INIT'})
+
+  return {
+    dispatch,
+    getState
+  }
 }
 
 function render() {
   const container = document.getElementById('container');
 }
+
+
+// In createStore.js, write a function called createStore that takes in a reducer function as an argument. We've provided the reducer functions for you so you can get a sense of how they'll work.
+// The createStore function should return an object with two methods - getState, dispatch.
+// getState should return the current state.
+// dispatch should take in an action, update the state using the reducer, and call the render function.
